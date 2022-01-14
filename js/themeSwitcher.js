ad5685dc6fdc37e2f2eb85ae7dcc219252ac7aa1
Romain Pontacq <romain@macbook-pro-de-romain.home>
2022-01-14 17:00:32 +0100

    home page terminé

--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -3,19 +3,42 @@ const body = document.body
 const links = document.getElementById("navigation")
 const topBarEl = document.getElementById("topbar-el")
 
-const themeSwitcher = document.querySelector('input[type=checkbox]')
-themeSwitcher.checked = false
+const tsBigScreen = document.getElementById("themeSwitcherBigScreen")
+tsBigScreen.checked = false
+const tsMobile = document.getElementById("themeSwitcherMobile")
+tsMobile.checked = false
 
-themeSwitcher.addEventListener("change", function(){
+tsBigScreen.addEventListener("change", function(){
     if(this.checked){
         body.style.backgroundColor = "rgba(27, 27, 27)"
-        // body.style.backgroundColor = "#3e4372"
         body.style.color = "#F7F7F7"
         r.style.setProperty('--textColor', "#F7F7F7")
         r.style.setProperty('--themeContent',"rgba( 42, 42, 42, 1 )")
-        // r.style.setProperty('--themeShadow',"0p 2px 8px 0 rgba(  12, 12, 12 , 0.37 )")
         r.style.setProperty('--themeShadow',"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px")
         r.style.setProperty('--gadgetColor', "rgb( 42, 42, 42)")
+        tsMobile.checked = true
+
+    }
+    else{
+        body.style.backgroundColor = "rgba(247, 247, 247, 1)"
+        body.style.color = "#1b1b1b"
+        r.style.setProperty('--textColor', "#1b1b1b")
+        r.style.setProperty('--themeContent', "rgba( 255, 255, 255, 1 )")
+        r.style.setProperty('--themeShadow',"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px")
+        r.style.setProperty('--gadgetColor', "rgb( 255, 255, 255)")
+        tsMobile.checked = false
+    }
+})
+
+tsMobile.addEventListener("change", function(){
+    if(this.checked){
+        body.style.backgroundColor = "rgba(27, 27, 27)"
+        body.style.color = "#F7F7F7"
+        r.style.setProperty('--textColor', "#F7F7F7")
+        r.style.setProperty('--themeContent',"rgba( 42, 42, 42, 1 )")
+        r.style.setProperty('--themeShadow',"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px")
+        r.style.setProperty('--gadgetColor', "rgb( 42, 42, 42)")
+        tsBigScreen.checked = true
 
     }
     else{
@@ -26,6 +49,7 @@ themeSwitcher.addEventListener("change", function(){
         r.style.setProperty('--themeContent', "rgba( 255, 255, 255, 1 )")
         r.style.setProperty('--themeShadow',"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px")
         r.style.setProperty('--gadgetColor', "rgb( 255, 255, 255)")
+        tsBigScreen.checked = false
     }
 })
 // - END - //
